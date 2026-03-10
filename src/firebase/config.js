ff7ecdf21bc2c7da2ec6b67e6fdafeae5ff00f36
신원정 <shinwonjung@sin-wonjeong-ui-MacBookAir.local>
2026-03-10 20:25:04 +0900

    환경변수 설정

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore"
 import { getAuth, GoogleAuthProvider } from "firebase/auth"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAS_jGOfvCp8lM9hwqfsSx9ktW59-LTWgI",
-  authDomain: "toeic-words-28d6f.firebaseapp.com",
-  projectId: "toeic-words-28d6f",
-  storageBucket: "toeic-words-28d6f.firebasestorage.app",
-  messagingSenderId: "372944395895",
-  appId: "1:372944395895:web:b31e233204dd90516b7072"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 }
 
 const app = initializeApp(firebaseConfig)
